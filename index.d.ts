db3527e7b289d24d834a47123fa4cc54136aac68
Kevin Schmidt <kevin@kswebentwicklung.de>
2022-04-12 16:16:16 +0200

    xDxD build

---
+++ index.d.ts
@@ -0,0 +1,22 @@
+export declare const theme: {
+    colors: {
+        brand: {
+            primary: {
+                50: string;
+                100: string;
+                150: string;
+            };
+            secondary: {
+                50: string;
+                100: string;
+                150: string;
+            };
+        };
+    };
+    fonts: {
+        body: string;
+        heading: string;
+        mono: string;
+    };
+};
+//# sourceMappingURL=index.d.ts.map
---
+++ theme/colors.d.ts
@@ -0,0 +1,16 @@
+declare const colors: {
+    brand: {
+        primary: {
+            50: string;
+            100: string;
+            150: string;
+        };
+        secondary: {
+            50: string;
+            100: string;
+            150: string;
+        };
+    };
+};
+export default colors;
+//# sourceMappingURL=colors.d.ts.map
