4cfe84989ed6e646903e61cbdd0cd18ba7216b21
Kevin Schmidt <kevin@kswebentwicklung.de>
2022-04-12 15:46:01 +0200

    typed

---
+++ theme/fonts.d.ts
@@ -0,0 +1,10 @@
+import "@fontsource/open-sans/400.css";
+import "@fontsource/open-sans/700.css";
+import "@fontsource/montserrat/500.css";
+declare const fonts: {
+    body: string;
+    heading: string;
+    mono: string;
+};
+export default fonts;
+//# sourceMappingURL=fonts.d.ts.map
---
+++ theme/index.d.ts
@@ -0,0 +1,23 @@
+declare const theme: {
+    colors: {
+        brand: {
+            primary: {
+                50: string;
+                100: string;
+                150: string;
+            };
+            secondary: {
+                50: string;
+                100: string;
+                150: string;
+            };
+        };
+    };
+    fonts: {
+        body: string;
+        heading: string;
+        mono: string;
+    };
+};
+export default theme;
+//# sourceMappingURL=index.d.ts.map
