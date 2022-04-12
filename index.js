efb777e1d319e128e76e6099d5e55af21286683c
Kevin Schmidt <kevin@kswebentwicklung.de>
2022-04-13 01:03:47 +0200

    extendTheme

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
+const { extendTheme } = require("@chakra-ui/react");
 const { colors } = require("./theme/colors");
 const { fonts, FontsLoader } = require("./theme/fonts");
 
-const theme = {
+const theme = extendTheme({
   colors: colors,
   fonts: fonts,
-};
+});
 
 exports.theme = theme;
 exports.FontsLoader = FontsLoader;
