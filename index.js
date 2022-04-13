4d4d47dd295d66223a36dcef0905ab8f2cc79689
Kevin Schmidt <kevin@kswebentwicklung.de>
2022-04-13 09:52:49 +0200

    styles and components defaults

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const colors = require("./theme/colors");
+const components = require("./theme/components");
 const fonts = require("./theme/fonts");
+const styles = require("./theme/styles");
 
 const theme = {
   colors: colors,
   fonts: fonts,
+  components: components,
+  styles: styles,
 };
 
 module.exports = theme;
---
+++ theme/components.js
@@ -0,0 +1,20 @@
+const components = {
+  Heading: {
+    variants: {
+      "with-line": {
+        position: "relative",
+        _before: {
+          content: "''",
+          position: "absolute",
+          bottom: "-1em",
+          right: "0",
+          bg: "brand.secondary.100",
+          width: "140px",
+          height: "6px",
+        },
+      },
+    },
+  },
+};
+
+module.exports = components;
